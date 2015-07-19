c78b180fd5eb2e4d4337c3417d1560a8654b13c9
Reid Philpot <reidphilpot@gmail.com>
2015-07-19 21:34:39 +0100

    refactor

--- editors.js
+++ editors.js
@@ -5,44 +5,43 @@ define(
 , function($) {
 
     function numberEditor(args) {
-      var $input
-      var defaultValue
-      var scope = this
+      var input
+        , defaultValue
 
       this.init = function () {
-        $input = $("<INPUT type=number class='editor-text' />")
+        input = $("<INPUT type=number class='editor-text' />")
 
-        if(args.column.min)  $input.attr('min', args.column.min)
-        if(args.column.max)  $input.attr('max', args.column.max)
-        if(args.column.step) $input.attr('step', args.column.step)
+        if(args.column.min)  input.attr('min', args.column.min)
+        if(args.column.max)  input.attr('max', args.column.max)
+        if(args.column.step) input.attr('step', args.column.step)
 
-        $input.bind("keydown.nav", function (e) {
+        input.bind("keydown.nav", function (e) {
           if (e.keyCode === $.ui.keyCode.LEFT || e.keyCode === $.ui.keyCode.RIGHT) {
             e.stopImmediatePropagation()
           }
         })
 
-        $input.appendTo(args.container)
-        $input.focus().select()
+        input.appendTo(args.container)
+        input.focus().select()
       }
 
       this.destroy = function () {
-        $input.remove()
+        input.remove()
       }
 
       this.focus = function () {
-        $input.focus()
+        input.focus()
       }
 
       this.loadValue = function (item) {
         defaultValue = item[args.column.field]
-        $input.val(defaultValue)
-        $input[0].defaultValue = defaultValue
-        $input.select()
+        input.val(defaultValue)
+        input[0].defaultValue = defaultValue
+        input.select()
       }
 
       this.serializeValue = function () {
-        return new Number($input.val())
+        return new Number(input.val())
       }
 
       this.applyValue = function (item, state) {
@@ -50,18 +49,18 @@ define(
       }
 
       this.isValueChanged = function () {
-        return (!($input.val() == "" && defaultValue == null)) && ($input.val() != defaultValue)
+        return (!(input.val() == "" && defaultValue == null)) && (input.val() != defaultValue)
       }
 
       this.validate = function () {
-        if (isNaN($input.val())) {
+        if (isNaN(input.val())) {
           return {
             valid: false
           , msg: "Please enter a valid integer"
           }
         }
 
-        if (outOfBounds($input.val())) {
+        if (outOfBounds(input.val())) {
           return {
             valid: false
           , msg: "Value out of bounds"
@@ -78,8 +77,8 @@ define(
 
       function outOfBounds(val) {
         if (
-          args.column.min != undefined && $input.val() < args.column.min ||
-          args.column.max != undefined && $input.val() > args.column.max
+          args.column.min != undefined && input.val() < args.column.min ||
+          args.column.max != undefined && input.val() > args.column.max
         ) return true
         return false
       }
